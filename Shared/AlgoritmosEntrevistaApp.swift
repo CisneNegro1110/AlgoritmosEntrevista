141f2d0e73758fc8e2ee6648a2f468a400f44ff0
JesúsFranciscoLeyva <leyva527@gmail.com>
2022-05-10 19:03:31 -0600

    Initial Commit

---
+++ Shared/AlgoritmosEntrevistaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlgoritmosEntrevistaApp.swift
+//  Shared
+//
+//  Created by Jesús Francisco Leyva Juárez on 10/05/22.
+//
+
+import SwiftUI
+
+@main
+struct AlgoritmosEntrevistaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
